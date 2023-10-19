9cac08d0f80ebd2d430c71d398d7d605119f18e8
石七画 <141410871+2023djash@users.noreply.github.com>
2023-10-20 00:12:23 +0800

    更新 Liqingzhao.js

--- Liqingzhao.js
+++ Liqingzhao.js
@@ -9,7 +9,7 @@ Yugg
 [rewrite_local]
 
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Liqingzhao.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/2023djash/Surge/main/Liqingzhao.js
 
 [mitm] 
 
