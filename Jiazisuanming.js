9b0387fa7762c65871e2d79fe9278dcc94739528
石七画 <141410871+2023djash@users.noreply.github.com>
2023-10-20 13:54:13 +0800

    更新 Jiazisuanming.js

--- Jiazisuanming.js
+++ Jiazisuanming.js
@@ -1,22 +1,11 @@
-/*************************
+/******************************
 
 脚本名称: 甲子算命
-使用声明：此脚本仅供学习与交流，请勿转载与贩卖！
-
-**************************
-
-[rewrite_local]
-
-^https:\/\/app.iyzbz.com\/app\/user\/selfinfo url script-response-body https://raw.githubusercontent.com/2023djash/Surge/main/Jiazisuanming.js
-
-[mitm] 
-
-hostname = app.iyzbz.com
 
 *******************************/
 
-var Yu = JSON.parse($response.body);
+var  = JSON.parse($response.body);
 data.memberLevel = 1;
-data.userName = "甲子算命";
+data.userName = "牛马算命";
 data.expireTime = "2023-13-32T25:61:41.000+08:00";
-$done({body : JSON.stringify(baby)});
+$done({body : JSON.stringify});
