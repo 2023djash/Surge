2da7e06da6dbfcb341350cafc7923a459524ddc0
石七画 <141410871+2023djash@users.noreply.github.com>
2023-10-20 13:54:30 +0800

    更新 Jiazisuanming.js

--- Jiazisuanming.js
+++ Jiazisuanming.js
@@ -6,6 +6,6 @@
 
 var  = JSON.parse($response.body);
 data.memberLevel = 1;
-data.userName = "牛马算命";
+data.userName = "甲子算命";
 data.expireTime = "2023-13-32T25:61:41.000+08:00";
 $done({body : JSON.stringify});
