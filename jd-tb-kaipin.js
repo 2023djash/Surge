384a3e2332298098f574cb2254ce0d4a1947c2a8
石七画 <141410871+2023djash@users.noreply.github.com>
2023-10-29 18:45:39 +0800

    更新 jd-tb-kaipin.js

--- jd-tb-kaipin.js
+++ jd-tb-kaipin.js
@@ -1,4 +1,7 @@
-// 2023-10-26 09:35
+/*
+脚本引用https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/myBlockAds.js
+*/
+// 2023-10-28 19:25
 
 if (!$response.body) $done({});
 const url = $request.url;
@@ -6,105 +9,66 @@ let body = $response.body;
 
 if (body) {
   switch (true) {
-    // 京东-开屏广告
-    case /^https:\/\/api\.m\.jd\.com\/client\.action\?functionId=start/.test(
-      url
-    ):
+    // 京东-个人主页
+    case /^https:\/\/api\.m\.jd\.com\/client\.action\?functionId=personinfoBusiness/.test(url):
       try {
         let obj = JSON.parse(body);
-        for (let i = 0; i < obj.images.length; i++) {
-          for (let j = 0; j < obj.images[i].length; j++) {
-            if (obj.images[i][j].showTimes) {
-              obj.images[i][j].showTimes = 0;
-              obj.images[i][j].onlineTime = "2040-01-01 00:00:00";
-              obj.images[i][j].referralsTime = "2040-01-01 23:59:59";
-              obj.images[i][j].time = 0;
+        if (obj?.floors?.length > 0) {
+          let newFloors = [];
+          for (let floor of obj.floors) {
+            const items = [
+              "bigSaleFloor",
+              // "iconToolFloor", // 京东农场 客服服务
+              // "newAttentionCard", // 关注的频道
+              "newBigSaleFloor", // 双十一
+              "noticeFloor", // 顶部横幅 会员优惠
+              "recommendfloor" // 专属推荐
+            ];
+            if (items?.includes(floor?.mId)) {
+              continue;
+            } else {
+              if (floor?.mId === "basefloorinfo") {
+                // 弹窗
+                if (floor?.data?.commonPopup) {
+                  delete floor.data.commonPopup;
+                }
+                // 弹窗
+                if (floor?.data?.commonWindows?.length > 0) {
+                  floor.data.commonWindows = [];
+                }
+              } else if (floor?.mId === "userinfo") {
+                // 顶部背景图
+                if (floor?.data?.bgImgInfo?.bgImg) {
+                  delete floor.data.bgImgInfo.bgImg;
+                }
+                // 开通plus会员卡片
+                // if (floor?.data?.newPlusBlackCard) {
+                // delete floor.data.newPlusBlackCard;
+                // }
+              }
+              newFloors.push(floor);
             }
           }
+          obj.floors = newFloors;
         }
-        obj.countdown = 100;
-        obj.showTimesDaily = 0;
+        body = JSON.stringify(obj);
+      } catch (error) {
+        console.log(`京东-个人主页, 出现异常: ` + error);
+      }
+      break;
+    // 京东-开屏广告
+    case /^https:\/\/api\.m\.jd\.com\/client\.action\?functionId=start/.test(url):
+      try {
+        let obj = JSON.parse(body);
+        if (obj?.images?.length > 0) {
+          obj.images = [];
+        }
+        if (obj?.showTimesDaily) {
+          obj.showTimesDaily = 0;
+        }
         body = JSON.stringify(obj);
       } catch (error) {
         console.log(`京东-开屏广告, 出现异常: ` + error);
-      }
-      break;
-    // 淘宝-开屏视频广告
-    case /^https:\/\/guide-acs\.m\.taobao\.com\/gw\/mtop\.taobao\.cloudvideo\.video\.query/.test(url):
-      try {
-        let obj = JSON.parse(body);
-        if (obj?.data?.duration) {
-          obj.data.duration = "0";
-        }
-        if (obj?.data?.resources?.length > 0) {
-          obj.data.resources = [];
-        }
-        if (obj?.data?.caches?.length > 0) {
-          obj.data.caches = [];
-        }
-        if (obj?.data?.respTimeInMs) {
-          obj.data.respTimeInMs = "5364633600000";
-        }
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`淘宝-开屏视频广告, 出现异常: ` + error);
-      }
-      break;
-    // 淘宝-开屏图片广告
-    case /^https:\/\/guide-acs\.m\.taobao\.com\/gw\/mtop\.taobao\.wireless\.home\.splash\.awesome\.get/.test(url):
-      try {
-        let obj = JSON.parse(body);
-        if (obj?.data?.containers?.splash_home_base) {
-          let splash = obj.data.containers.splash_home_base;
-          if (splash?.base?.sections?.length > 0) {
-            for (let items of splash.base.sections) {
-              if ("taobao-splash" in items.bizData) {
-                if (items["taobao-splash"]?.data?.length > 0) {
-                  for (let item of items.bizData["taobao-splash"].data) {
-                    item.times = "0";
-                    item.hotStart = "false";
-                    item.haveVoice = "false";
-                    item.hideTBLogo = "false";
-                    item.enable4G = "false";
-                    item.coldStart = "false";
-                    item.waitTime = "0";
-                    item.imgUrl = "";
-                    item.startTime = "5364633600000";
-                    item.endTime = "5364719999000";
-                    item.gmtStart = "2140-01-01 00:00:00";
-                    item.gmtEnd = "2140-01-01 23:59:59";
-                    item.gmtStartMs = "5364633600000";
-                    item.gmtEndMs = "5364719999000";
-                  }
-                }
-              }
-            }
-          }
-        }
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`淘宝-开屏图片广告, 出现异常: ` + error);
-      }
-      break;
-    // 淘宝-开屏活动
-    case /^https:\/\/poplayer\.template\.alibaba\.com\/\w+\.json/.test(url):
-      try {
-        let obj = JSON.parse(body);
-        if (obj?.res?.images?.length > 0) {
-          obj.res.images = [];
-        }
-        if (obj?.res?.videos?.length > 0) {
-          obj.res.videos = [];
-        }
-        if (obj?.enable) {
-          obj.enable = false;
-        }
-        if (obj?.mainRes?.images?.length > 0) {
-          obj.mainRes.images = [];
-        }
-        body = JSON.stringify(obj);
-      } catch (error) {
-        console.log(`淘宝-开屏活动, 出现异常: ` + error);
       }
       break;
   }
